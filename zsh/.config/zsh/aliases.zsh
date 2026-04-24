a60b03dce57c351175c73ee82af9bd417f37292a
Chris John <132337461+Git-With-Chris@users.noreply.github.com>
2026-04-24 13:57:41 +1000

    Updated Aliases & Ignore file

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,7 +2,7 @@
 alias ls='eza --icons=always --group-directories-first'
 alias ll='eza -lh --icons=always --group-directories-first'
 alias la='eza -lah --icons=always --group-directories-first'
-alias tree='eza --tree --icons=always --group-directories-first'
+alias lt='eza --tree --icons=always --group-directories-first'
 
 # Package: bat (cat replacement)
 alias cat='bat'
@@ -15,3 +15,7 @@ alias lgit='lazygit'
 
 # Show all aliases
 alias showal='bat ~/.config/zsh/aliases.zsh'
+
+# Package: Jupyter Notebook and Jupyter Lab
+alias jn="jupyter notebook"
+alias jl="jupyter lab"
